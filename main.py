776f0a99be338dc529bc00e397633ca8b731b8e1
David Castro <davidcastrosi@hotmail.com>
2022-06-08 16:41:14 -0600

    Adding movement by pressing keys

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-from turtle import Turtle, Screen
+from turtle import Screen, onkey
 import time
+from snake import Snake
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake game")
 screen.tracer(0)
-x_coordinates = [0, -20 , -40]
+x_coordinates = [0, -20, -40]
 
 
 #  snake creation
-segments = []
-for x in range(len(x_coordinates)):
-    snake = Turtle(shape="square")
-    snake.color("white")
-    snake.penup()
-    snake.goto(x_coordinates[x], 0)
-    segments.append(snake)
+snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up,'Up')
+screen.onkey(snake.down,'Down')
+screen.onkey(snake.left,'Left')
+screen.onkey(snake.right,'Right')
 
 
 game_is_on = True
@@ -26,14 +27,7 @@ while game_is_on:
     time.sleep(0.1)
 
     # move the snake
-    for seg_num in range(len(segments)-1 ,0 ,-1 ):
-        segments[seg_num].goto(segments[seg_num-1].xcor(), segments[seg_num-1].ycor())
-    segments[0].forward(20)
-
-
-
-
-
+    snake.move()
 
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,18 +1,45 @@
-from turtle import Turtle, Screen
+from turtle import Turtle
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
-
-class Snake():
+class Snake:
     def __init__(self):
-        self.x_coordinates = [0, -20 , -40]
         self.segments = []
-        for x in range(len(self.x_coordinates)):
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITION:
             snake = Turtle(shape="square")
             snake.color("white")
             snake.penup()
-            snake.goto(self.x_coordinates[x], 0)
-            self.segments.append(snake) 
+            snake.goto(position)
+            self.segments.append(snake)
 
     def move(self):
-        for seg_num in range(len(self.segments)-1 ,0 ,-1 ):
-            self.segments[seg_num].goto(self.segments[seg_num-1].xcor(), self.segments[seg_num-1].ycor())
-        self.segments[0].forward(20)
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            self.segments[seg_num].goto(
+                self.segments[seg_num - 1].xcor(), self.segments[seg_num - 1].ycor()
+            )
+        self.head.forward(MOVE_DISTANCE)
+
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
